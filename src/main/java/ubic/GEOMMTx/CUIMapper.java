56cc153052ea85b56b2c5d6c4875d8fa1f37b356
lfrench <>
2007-10-17 03:34:47 +0000

    added comment

--- src/main/java/ubic/GEOMMTx/CUIMapper.java
+++ src/main/java/ubic/GEOMMTx/CUIMapper.java
@@ -3,5 +3,12 @@ package ubic.GEOMMTx;
 import java.util.Collection;
 
 public interface CUIMapper {
+    /**
+     * Given a CUI, and UMLS codes and sources attatched to it, return its URI
+     * 
+     * @param CUI UMLS concept identifier
+     * @param sourceCodes UMLS code+source pairings 
+     * @return a URI
+     */
     public String convert( String CUI, Collection<UMLSSourceCode> sourceCodes );
 }
