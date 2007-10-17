87ba65249f86db58a9f4fd17ae415a7882904093
lfrench <>
2007-10-17 03:35:36 +0000

    renamed

--- src/main/java/ubic/GEOMMTx/mappers/AbstractToUMLSMapper.java
+++ src/main/java/ubic/GEOMMTx/mappers/AbstractToUMLSMapper.java
@@ -10,13 +10,17 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import ubic.GEOMMTx.CUIMapper;
 import ubic.GEOMMTx.UMLSSourceCode;
 
-public abstract class AbstractMapper {
-    protected static Log log = LogFactory.getLog( AbstractMapper.class );
+/**
+ * This class is for mapping to an ontology that has CUI's in it, here you are going backwards from an ontology to UMLS
+ * 
+ * @author Leon
+ */
+public abstract class AbstractToUMLSMapper implements CUIMapper {
+    protected static Log log = LogFactory.getLog( AbstractToUMLSMapper.class );
 
-    protected static  String MAIN_URL=null;
-    
     //
     protected Map<String, String> CUIMap;
 
@@ -24,13 +28,14 @@ public abstract class AbstractMapper {
     // loadFromDisk
     // loadfromOntology
 
-    public AbstractMapper() {
+    public AbstractToUMLSMapper() {
         super();
         try {
+            log.info( "getting " + getMainURL() );
             loadFromDisk();
             log.info( "loaded from disk, mappings:" + CUIMap.size() );
         } catch ( Exception e ) {
-            log.info( "can't load from disk, loading from ontology");
+            log.info( "can't load from disk, loading from ontology" );
             loadFromOntology();
             log.info( "loaded, mappings:" + CUIMap.size() );
             save();
@@ -39,21 +44,33 @@ public abstract class AbstractMapper {
     }
 
     public void loadFromDisk() throws Exception {
-        ObjectInputStream o = new ObjectInputStream(new FileInputStream(this.getClass().getName()));
-        CUIMap = (Map<String, String>)o.readObject();
+        ObjectInputStream o = new ObjectInputStream( new FileInputStream( getFileName() ) );
+        CUIMap = ( Map<String, String> ) o.readObject();
         o.close();
-        
+
     }
 
     abstract void loadFromOntology();
 
+    abstract String getMainURL();
+
+    /**
+     * Converts a UMLS concept into URI for a specific ontology
+     * 
+     * @param CUI UMLS concept identifier
+     * @return
+     */
     public String convert( String CUI, Collection<UMLSSourceCode> sourceCodes ) {
         return CUIMap.get( CUI );
     }
 
+    public String getFileName() {
+        return this.getClass().getName() + ".mappings";
+    }
+
     public void save() {
         try {
-            ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream(this.getClass().getName()));
+            ObjectOutputStream o = new ObjectOutputStream( new FileOutputStream( getFileName() ) );
             o.writeObject( CUIMap );
             o.close();
         } catch ( Exception e ) {
