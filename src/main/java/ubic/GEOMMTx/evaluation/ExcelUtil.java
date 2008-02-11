e5324bb05e93162e325266ebd2b92ce517c8d67c
lfrench <>
2008-02-11 23:53:33 +0000

    Added a method for getting sheet from file

--- src/main/java/ubic/GEOMMTx/evaluation/ExcelUtil.java
+++ src/main/java/ubic/GEOMMTx/evaluation/ExcelUtil.java
@@ -1,11 +1,28 @@
 package ubic.GEOMMTx.evaluation;
 
+import java.io.FileInputStream;
+
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 public class ExcelUtil {
+    public static HSSFSheet getSheetFromFile( String filename, String sheetName ) throws Exception {
+        POIFSFileSystem fs = new POIFSFileSystem( new FileInputStream( filename ) );
+        HSSFWorkbook wb = new HSSFWorkbook( fs );
+        return wb.getSheet( sheetName );
+    }
+
+    public static String getValue( HSSFSheet sheet, int row, int col ) {
+        if ( col > 255 ) {
+            throw new RuntimeException( "Column position is over 255" );
+        }
+        return getValue( sheet, row, ( short ) col );
+    }
+
     public static String getValue( HSSFSheet sheet, int row, short col ) {
         if ( sheet.getRow( row ) == null ) return null;
         HSSFCell cell = sheet.getRow( row ).getCell( col );
@@ -27,12 +44,13 @@ public class ExcelUtil {
     }
 
     public static void setValue( HSSFSheet sheet, int row, int col, String value ) {
-        setValue(sheet, row, (short)col, value);
+        setValue( sheet, row, ( short ) col, value );
     }
+
     public static void setValue( HSSFSheet sheet, int row, short col, String value ) {
         HSSFRow r = sheet.createRow( row );
         HSSFCell c = r.createCell( col );
         c.setCellType( HSSFCell.CELL_TYPE_STRING );
-        c.setCellValue(new HSSFRichTextString(value));
+        c.setCellValue( new HSSFRichTextString( value ) );
     }
 }
