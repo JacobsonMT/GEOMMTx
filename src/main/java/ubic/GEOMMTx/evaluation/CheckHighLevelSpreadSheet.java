54a05f33f5614e2e081f2cf1e04303e3b3ae94ac
lfrench <>
2008-08-09 22:49:04 +0000

    Code to creat high level spreadsheet

---
+++ src/main/java/ubic/GEOMMTx/evaluation/CheckHighLevelSpreadSheet.java
@@ -0,0 +1,63 @@
+package ubic.GEOMMTx.evaluation;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+/*
+ * Call this from CompareToManual.java
+ */
+public class CheckHighLevelSpreadSheet extends CreateSpreadSheet {
+
+    public CheckHighLevelSpreadSheet( String filename ) throws Exception {
+        super( filename, new CheckHighLevelSchema() );
+    }
+
+    public void populate( Map<String, Set<String>> newPredictions, Map<String,String> labels, int size ) throws Exception {
+
+        List<String> datasets = new LinkedList<String>( newPredictions.keySet() );
+
+        Collections.shuffle(datasets, new Random(1));
+        
+        System.out.println( datasets );
+        // pick random ones
+        // through the set to list operations, assume it is random
+        // write out
+
+        int row = 0;
+        for ( int i = 0; i < size; i++ ) {
+            row++;
+            String dataset = datasets.get( i );
+            // ExcelUtil.setValue( spreadsheet, row, schema.getPosition( "Dataset" ), dataset );
+            // for each URL
+            
+            //if its empty don't do it
+            if (newPredictions.get( dataset ).isEmpty()) {
+                size++;
+                row--;
+            }
+            for ( String URL : newPredictions.get( dataset ) ) {
+                row++;
+                ExcelUtil.setFormula( spreadsheet, row, schema.getPosition( "Dataset" ), "HYPERLINK(\"http://bioinformatics.ubc.ca/Gemma/expressionExperiment/showExpressionExperiment.html?id="+dataset+"\",\""+ dataset+"\")");
+                ExcelUtil.setValue( spreadsheet, row, schema.getPosition( "URL" ), URL );
+                ExcelUtil.setValue( spreadsheet, row, schema.getPosition( "Name" ), labels.get(URL));
+                
+            }
+
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main( String[] args ) throws Exception {
+        // CheckHighLevelSpreadSheet test = new CheckHighLevelSpreadSheet( "ttx.xls" );
+        // test.save();
+        // log.info( "Done!" );
+
+    }
+
+}
