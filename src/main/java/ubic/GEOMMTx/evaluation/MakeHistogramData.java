03006405d7e6ce19fb9bc78358668bddf4ed6cbf
lfrench <>
2008-08-13 18:47:23 +0000

    re-worked, could use further refactoring

--- src/main/java/ubic/GEOMMTx/evaluation/MakeHistogramData.java
+++ src/main/java/ubic/GEOMMTx/evaluation/MakeHistogramData.java
@@ -1,6 +1,8 @@
 package ubic.GEOMMTx.evaluation;
 
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,7 +12,10 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-import ubic.GEOMMTx.Text2OwlModelTools;
+import ubic.GEOMMTx.SetupParameters;
+import ubic.basecode.dataStructure.matrix.DenseDoubleMatrix2DNamed;
+import ubic.basecode.dataStructure.matrix.DoubleMatrixNamed;
+import ubic.basecode.io.writer.MatrixWriter;
 import ubic.gemma.ontology.OntologyTools;
 
 import com.hp.hpl.jena.query.Query;
@@ -30,7 +35,7 @@ public class MakeHistogramData {
             current = new CopyOnWriteArraySet<String>();
             hash.put( key, current );
         }
-        current.add( value );
+        if ( value != null ) current.add( value );
     }
 
     // convert a hash of sets to an vector of the set sizes
@@ -43,20 +48,85 @@ public class MakeHistogramData {
     }
 
     // so we get all the experiment to concept pairings that have mapped terms and then make a vector
-    public static void makeHistoGrams(String file) throws Exception {
+    public static void makeHistoGrams( String file ) throws Exception {
         Map<String, Set<String>> expToConcept = new HashMap<String, Set<String>>();
         Map<String, Set<String>> conceptToExp = new HashMap<String, Set<String>>();
+        Set<String> titles = new HashSet<String>();
+        Set<String> URIs = new HashSet<String>();
+        Map<String, String> labelMap = new HashMap<String, String>();
 
         Model model = ModelFactory.createDefaultModel();
         model.read( new FileInputStream( file ), null );
+        model.read( new FileInputStream( SetupParameters.gemmaTitles ), null );
 
-        String queryString = "PREFIX leon: <http://www.purl.org/leon/umls#>                                            \r\n"
-                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>                                \r\n"
+        String queryString = "PREFIX leon: <http://www.purl.org/leon/umls#>                                            \n"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>                                     \n"
+                + "SELECT DISTINCT ?mapping ?label \n"
+                + "WHERE {                                                                                  \n"
+                + "    ?mention leon:mappedTerm ?mapping .                                                            \n"
+                + "    ?mention rdfs:label ?label .                                                         \n"
+                + " }                                                                                        \n";
+        Query q = QueryFactory.create( queryString );
+        QueryExecution qexec = QueryExecutionFactory.create( q, model );
+        ResultSet results = qexec.execSelect();
+        while ( results.hasNext() ) {
+            QuerySolution soln = results.nextSolution();
+            String mapping = OntologyTools.varToString( "mapping", soln );
+            String label = "\"" + OntologyTools.varToString( "label", soln ) + "\"";
+            labelMap.put( mapping, label );
+        }
+        qexec.close();
+
+        queryString = "PREFIX leon: <http://www.purl.org/leon/umls#>                                            \r\n"
                 + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>                                     \r\n"
-                + "PREFIX gss: <http://www.w3.org/2001/11/IsaViz/graphstylesheets#>                         \r\n"
-                + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>                                          \r\n"
-                + "PREFIX rs: <http://www.w3.org/2001/sw/DataAccess/tests/result-set#>                      \r\n"
-                + "SELECT ?dataset ?geoLabel ?label  ?mapping                                             \r\n"
+                + "PREFIX dc: <http://purl.org/dc/elements/1.1/>                                           \r\n"
+                + "SELECT DISTINCT ?dataset ?geoLabel ?mapping ?title                                        \r\n"
+                + "WHERE {                                                                                  \r\n"
+                + "    ?dataset leon:describedBy ?description .                                             \r\n"
+                + "    ?dataset rdfs:label ?geoLabel .                                                      \r\n"
+                + "    OPTIONAL {?dataset dc:title ?title}  .                                                           \r\n"
+                + "    OPTIONAL{                                                                             \n"
+                + "      ?description leon:hasPhrase ?phrase .                                                \r\n"
+                + "      ?phrase leon:hasMention ?mention .                                                   \r\n"
+                + "      ?mention leon:mappedTerm ?mapping .                                                  \r\n"
+                + "      ?mention rdfs:label ?label .                                                         \r\n"
+                + "} }                                                                                        \r\n";
+
+        q = QueryFactory.create( queryString );
+        qexec = QueryExecutionFactory.create( q, model );
+        try {
+            results = qexec.execSelect();
+            while ( results.hasNext() ) {
+                QuerySolution soln = results.nextSolution();
+                String dataset = OntologyTools.varToString( "dataset", soln );
+                String geoLabel = OntologyTools.varToString( "geoLabel", soln );
+                String mapping = OntologyTools.varToString( "mapping", soln );
+                String label = labelMap.get( mapping );
+                String title = "\"" + OntologyTools.varToString( "title", soln ) + " [" + geoLabel + "]\"";
+                titles.add( title );
+                if ( mapping != null ) {
+                    URIs.add( mapping );
+                    addToSetMap( conceptToExp, label + "|" + mapping, geoLabel );
+                    addToSetMap( expToConcept, geoLabel, label + "|" + mapping );
+                } else {
+                    addToSetMap( expToConcept, geoLabel, null );
+                }
+            }
+        } finally {
+            qexec.close();
+        }
+        System.out.println( "URIs:" + URIs.size() );
+        Set<String> labels = new HashSet<String>();
+
+        for ( String URI : URIs ) {
+            labels.add( labelMap.get( URI ) );
+        }
+        System.out.println( "labels:" + labels.size() );
+
+        queryString = "PREFIX leon: <http://www.purl.org/leon/umls#>                                            \r\n"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>                                     \r\n"
+                + "PREFIX dc: <http://purl.org/dc/elements/1.1/>                                           \r\n"
+                + "SELECT DISTINCT ?dataset ?geoLabel ?mapping  ?title                                                  \r\n"
                 + "WHERE {                                                                                  \r\n"
                 + "    ?dataset leon:describedBy ?description .                                             \r\n"
                 + "    ?dataset rdfs:label ?geoLabel .                                                      \r\n"
@@ -64,20 +134,33 @@ public class MakeHistogramData {
                 + "    ?phrase leon:hasMention ?mention .                                                   \r\n"
                 + "    ?mention leon:mappedTerm ?mapping .                                                  \r\n"
                 + "    ?mention rdfs:label ?label .                                                         \r\n"
-                + "}                                                                                        \r\n";
+                + "    OPTIONAL {?dataset dc:title ?title} .                                                           \r\n"
+                + " }                                                                                        \r\n";
 
-        Query q = QueryFactory.create( queryString );
-        QueryExecution qexec = QueryExecutionFactory.create( q, model );
+        DoubleMatrixNamed<String, String> resultMatrix = new DenseDoubleMatrix2DNamed<String, String>( expToConcept
+                .size(), conceptToExp.size() );
+        resultMatrix.setRowNames( new ArrayList<String>( titles ) );
+        resultMatrix.setColumnNames( new ArrayList<String>( labels ) );
+
+        q = QueryFactory.create( queryString );
+        qexec = QueryExecutionFactory.create( q, model );
         try {
-            ResultSet results = qexec.execSelect();
+            results = qexec.execSelect();
+            int c = 0;
             while ( results.hasNext() ) {
                 QuerySolution soln = results.nextSolution();
+
                 String geoLabel = OntologyTools.varToString( "geoLabel", soln );
-                String mentionURL = OntologyTools.varToString( "mapping", soln );
-                String label = OntologyTools.varToString( "label", soln );
-                addToSetMap( conceptToExp, label+"|"+mentionURL, geoLabel );
-                addToSetMap( expToConcept, geoLabel, label );
+                String title = "\"" + OntologyTools.varToString( "title", soln ) + " [" + geoLabel + "]\"";
+
+                String mapping = OntologyTools.varToString( "mapping", soln );
+                String label = labelMap.get( mapping );
+
+                Double count = resultMatrix.getByKeys( title, label );
+                resultMatrix.setByKeys( title, label, 1.0 );
+                c++;
             }
+            System.out.println( "c=" + c );
         } finally {
             qexec.close();
         }
@@ -87,23 +170,54 @@ public class MakeHistogramData {
 
         // remove dupes within experiments - hash->set
 
+        // System.out.println( "Concept to experiment" );
+        // System.out.println( arrayToRString( getCounts( conceptToExp ) ) );
+        // printMapSizes( conceptToExp );
+        // System.out.println( "Experiment to Concept" );
+        // System.out.println( arrayToRString( getCounts( expToConcept ) ) );
+        // printMapSizes( expToConcept );
+
         System.out.println( "Concept to experiment" );
         System.out.println( arrayToRString( getCounts( conceptToExp ) ) );
-        printMapSizes(conceptToExp);
         System.out.println( "Experiment to Concept" );
         System.out.println( arrayToRString( getCounts( expToConcept ) ) );
-        printMapSizes(expToConcept);
-        
+        System.out.println( "Number of concepts:" + conceptToExp.size() );
+        System.out.println( "Number of experiments:" + expToConcept.size() );
+
+        int total = 0;
+        for ( Set<String> x : expToConcept.values() ) {
+            total += x.size();
+        }
+        System.out.println( "Total from exp:" + total );
+
+        total = 0;
+        for ( Set<String> x : conceptToExp.values() ) {
+            total += x.size();
+        }
+        System.out.println( "Total from concepts:" + total );
+
+        writeRTable( "ExperimentToConceptMatrix.txt", resultMatrix );
+
+        System.out.println( "Matrix wrote" );
     }
 
-    public static void printMapSizes(Map<String, Set<String>> map) {
+    public static void writeRTable( String filename, DoubleMatrixNamed<String, String> matrix ) throws Exception {
+        // write it out for R
+        FileWriter fOut = new FileWriter( filename );
+        MatrixWriter matWriter = new MatrixWriter( fOut );
+
+        matWriter.setTopLeft( "" );
+        matWriter.writeMatrix( matrix, true );
+        fOut.close();
+    }
+
+    public static void printMapSizes( Map<String, Set<String>> map ) {
         for ( String key : map.keySet() ) {
-            //System.out.println("\""+key +"\","+ map.get( key ).size() );
-            System.out.println(key + "|"+ map.get( key ).size() );
+            // System.out.println("\""+key +"\","+ map.get( key ).size() );
+            System.out.println( key + "|" + map.get( key ).size() );
         }
     }
-    
-    
+
     // make a string for R read.table
     public static String arrayToRString( Object[] array ) {
         String result = Arrays.asList( array ).toString();
@@ -112,8 +226,9 @@ public class MakeHistogramData {
         result = result.replaceAll( "\\]", "" );
         return result;
     }
-    
+
     public static void main( String[] args ) throws Exception {
-        makeHistoGrams( "mergedRDFBirnLexUpdate.afterUseless.rdf");
+        makeHistoGrams( "mergedRDFBirnLexUpdate.afterUseless.rdf" );
+        // makeHistoGrams( "mergedRDF.rejected.removed.rdf" );
     }
 }
