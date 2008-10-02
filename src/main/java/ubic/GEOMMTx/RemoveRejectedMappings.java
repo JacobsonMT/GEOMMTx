77451cb76e35b014fec30826fdb418461a22fc81
lfrench <>
2008-10-02 22:09:42 +0000

    added remove for experimental factors and URL's that are null

--- src/main/java/ubic/GEOMMTx/RemoveRejectedMappings.java
+++ src/main/java/ubic/GEOMMTx/RemoveRejectedMappings.java
@@ -13,6 +13,8 @@ import org.apache.commons.logging.LogFactory;
 import ubic.GEOMMTx.evaluation.CUIIRIPair;
 import ubic.GEOMMTx.evaluation.CUISUIPair;
 import ubic.GEOMMTx.evaluation.EvaluatePhraseToCUISpreadsheet;
+import ubic.gemma.ontology.BirnLexOntologyService;
+import ubic.gemma.ontology.FMAOntologyService;
 
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
@@ -26,6 +28,7 @@ import com.hp.hpl.jena.rdf.model.Resource;
 
 public class RemoveRejectedMappings {
     protected static Log log = LogFactory.getLog( RemoveRejectedMappings.class );
+
     Set<CUISUIPair> rejectedCUISUIPairs;
     Set<CUIIRIPair> rejectedCUIIRIPairs;
     Set<String> frequentURLs;
@@ -47,8 +50,19 @@ public class RemoveRejectedMappings {
     }
 
     public int removeFrequentURLs( Model model ) {
+        return removeMentionsURLs( model, frequentURLs );
+    }
+
+    /**
+     * remove mentions that have a mapped term corresponding to one of the given URL's
+     * 
+     * @param model the rdf model to remove mentions from
+     * @param URLs the urls to remove
+     * @return
+     */
+    public int removeMentionsURLs( Model model, Set<String> URLs ) {
         int howMany = 0;
-        for ( String URL : frequentURLs ) {
+        for ( String URL : URLs ) {
             Resource resource = model.createResource( URL );
             // list all the mentions
             ResIterator mentionIterator = model.listResourcesWithProperty( Vocabulary.mappedTerm, resource );
@@ -57,6 +71,58 @@ public class RemoveRejectedMappings {
             howMany += mentionSet.size();
         }
         return howMany;
+    }
+
+    public int removeBIRNLexFMANulls( Model model ) {
+        // need a list of all the appearing URL's
+        Set<String> removeURIs = new HashSet<String>();
+
+        String queryString = "PREFIX leon: <http://www.purl.org/leon/umls#>\n"
+                + "SELECT DISTINCT ?url \n                                                                                       "
+                + "WHERE {\n                                                                                                 "
+                + "   ?mention leon:" + Vocabulary.mappedTerm.getLocalName()
+                + " ?url .\n                                                                        " + "}";
+
+        // load FMA and birnlex
+        FMAOntologyService FMA = new FMAOntologyService();
+        BirnLexOntologyService BIRN = new BirnLexOntologyService();
+        FMA.init( true );
+        BIRN.init( true );
+        while ( !( FMA.isOntologyLoaded() && BIRN.isOntologyLoaded() ) ) {
+            try {
+                Thread.sleep( 2500 );
+            } catch ( Exception e ) {
+                e.printStackTrace();
+            }
+        }
+        log.info( "FMA and BIRNLex Ontologies loaded" );
+
+        Query q = QueryFactory.create( queryString );
+        QueryExecution qexec = QueryExecutionFactory.create( q, model );
+
+        int count = 0;
+
+        ResultSet results = qexec.execSelect();
+        while ( results.hasNext() ) {
+            QuerySolution soln = results.nextSolution();
+            Resource urlR = soln.getResource( "url" );
+            String URI = urlR.getURI();
+            // go into FMA and birnlex and check if it's missing
+            // if its then add it to the set
+            if ( URI.contains( "/owl/FMA#" ) && FMA.getTerm( URI ) == null ) {
+                removeURIs.add( URI );
+                log.info( URI );
+                count++;
+            }
+            if ( URI.contains( "birnlex" ) && BIRN.getTerm( URI ) == null ) {
+                removeURIs.add( URI );
+                log.info( URI );
+                count++;
+            }
+            // else its not FMA or birnlex
+        }
+        log.info( "number of null URL's:" + count );
+        return removeMentionsURLs( model, removeURIs );
     }
 
     public int removeCUIIRIPairs( Model model ) {
@@ -91,9 +157,9 @@ public class RemoveRejectedMappings {
         String queryString = "PREFIX leon: <http://www.purl.org/leon/umls#>\n"
                 + "SELECT  ?mention ?score \n                                                                                       "
                 + "WHERE {\n                                                                                                 "
-                + "   ?mention leon:" + Vocabulary.hasScore.getLocalName() + " ?score .\n                                 "
-                + "   FILTER (?score <" + minScore + ")                                                            "
-                + "}";
+                + "   ?mention leon:" + Vocabulary.hasScore.getLocalName()
+                + " ?score .\n                                 " + "   FILTER (?score <" + minScore
+                + ")                                                            " + "}";
 
         log.info( queryString );
 
@@ -134,6 +200,23 @@ public class RemoveRejectedMappings {
         return howMany;
     }
 
+    private int removeExperimentalFactors( Model model ) {
+        String queryString = "PREFIX rdf: <http://www.w3.org/2000/01/rdf-schema#>"
+                + "PREFIX leon: <http://www.purl.org/leon/umls#>\n                              "
+                + "\n                                                            "
+                + "SELECT DISTINCT ?dataset ?description ?mention\n                                                            "
+                + "WHERE {\n                                                            "
+                + "    ?dataset leon:describedBy ?description .\n                                                            "
+                + "    ?description leon:hasPhrase ?phrase .\n                                                            "
+                + "    ?phrase leon:hasMention ?mention .\n                                                            "
+                + "    FILTER regex(str(?description), \"experimentalFactor\") }";
+        Query q = QueryFactory.create( queryString );
+        QueryExecution qexec = QueryExecutionFactory.create( q, model );
+
+        ResultSet results = qexec.execSelect();
+        return removeMentions( model, results );
+    }
+
     private int removeMentions( Model model, ResultSet results ) {
         Set<Resource> affectedMentions = new HashSet<Resource>();
 
@@ -155,30 +238,39 @@ public class RemoveRejectedMappings {
         // System.out.println("CUI+IRI:"+remove.removeCUIIRIPairs( model ));
         // model.write( new FileWriter( "656.rejected.rdf" ) );
 
-        // Model model = Text2OwlModelTools.loadModel( "mergedRDFBirnLexUpdate.rdf" );
-        // System.out.println( "Frequent Useless URLs:" + remove.removeFrequentURLs( model ) );
-        // System.out.println( "CUI+SUI:" + remove.removeCUISUIPairs( model ) );
-        // System.out.println( "CUI+IRI:" + remove.removeCUIIRIPairs( model ) );
-        // model.write( new FileWriter( "mergedRDFBirnLexUpdate.afterrejected.rdf" ) );
+//        Model model = Text2OwlModelTools.loadModel( "mergedRDFBirnLexUpdate.rdf" );
+//        System.out.println( "Experimental factors:" + remove.removeExperimentalFactors( model ) );
+//        model.write( new FileWriter( "mergedRDFBirnLexUpdateNoExp.rdf" ) );
+//        log.info( "model wrote" );
 
-//        Model model = Text2OwlModelTools.loadModel( "mergedRDFBirnLexUpdate.afterrejected.rdf" );
-//        System.out.println( "Frequent Useless URLs:" + remove.removeFrequentURLs( model ) );
-//        model.write( new FileWriter( "mergedRDFBirnLexUpdate.afterUseless2.rdf" ) );
+//         Model model = Text2OwlModelTools.loadModel( "mergedRDFBirnLexUpdateNoExp.rdf" );
+//         System.out.println( "CUI+SUI:" + remove.removeCUISUIPairs( model ) );
+//         System.out.println( "CUI+IRI:" + remove.removeCUIIRIPairs( model ) );
+//         System.out.println( "Null IRIs:" + remove.removeBIRNLexFMANulls( model ) );
+//         model.write( new FileWriter( "mergedRDFBirnLexUpdate.afterrejected.rdf" ) );
+//         log.info("model wrote");
 
+         Model model = Text2OwlModelTools.loadModel( "mergedRDFBirnLexUpdate.afterrejected.rdf" );
+         System.out.println( "Frequent Useless URLs:" + remove.removeFrequentURLs( model ) );
+         model.write( new FileWriter( "mergedRDFBirnLexUpdate.afterUseless.rdf" ) );
+         log.info( "model wrote" );
+
+        
+        
         // Model model = Text2OwlModelTools.loadModel( "296.fix.rdf" );
         // System.out.println( "Frequent Useless URLs:" + remove.removeFrequentURLs( model ) );
         // model.write( new FileWriter( "296.afterUseless.rdf" ) );
 
-         Model model = Text2OwlModelTools.loadModel( "mergedRDF.rdf" );
-         System.out.println( "Frequent Useless URLs:" + remove.removeFrequentURLs( model ) );
-         System.out.println( "CUI+SUI:" + remove.removeCUISUIPairs( model ) );
-         System.out.println( "CUI+IRI:" + remove.removeCUIIRIPairs( model ) );
-         model.write( new FileWriter( "mergedRDF.rejected.removed.rdf" ) );
+        // Model model = Text2OwlModelTools.loadModel( "mergedRDF.rdf" );
+        // System.out.println( "Frequent Useless URLs:" + remove.removeFrequentURLs( model ) );
+        // System.out.println( "CUI+SUI:" + remove.removeCUISUIPairs( model ) );
+        // System.out.println( "CUI+IRI:" + remove.removeCUIIRIPairs( model ) );
+        // model.write( new FileWriter( "mergedRDF.rejected.removed.rdf" ) );
 
-//        Model model = Text2OwlModelTools.loadModel( "mergedRDF.rejected.removed.rdf" );
-//        int minScore = 1000;
-//        System.out.println( "Below score of " + minScore + ":" + remove.removeLowScores( model, minScore ) );
-//        model.write( new FileWriter( "mergedRDF.rejected.removed."+minScore+".rdf" ) );
+        // Model model = Text2OwlModelTools.loadModel( "mergedRDF.rejected.removed.rdf" );
+        // int minScore = 1000;
+        // System.out.println( "Below score of " + minScore + ":" + remove.removeLowScores( model, minScore ) );
+        // model.write( new FileWriter( "mergedRDF.rejected.removed."+minScore+".rdf" ) );
 
     }
 }
