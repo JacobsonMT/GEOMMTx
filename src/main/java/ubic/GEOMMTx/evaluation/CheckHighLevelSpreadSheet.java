7eacadd317201b482f32bf54f5d15094da196a15
lfrench <>
2008-12-09 19:51:21 +0000

    mostly formating

--- src/main/java/ubic/GEOMMTx/evaluation/CheckHighLevelSpreadSheet.java
+++ src/main/java/ubic/GEOMMTx/evaluation/CheckHighLevelSpreadSheet.java
@@ -7,6 +7,11 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+
+import ubic.GEOMMTx.HashMapStringSet;
+import ubic.GEOMMTx.SetupParameters;
+
 /*
  * Call this from CompareToManual.java
  */
@@ -16,15 +21,16 @@ public class CheckHighLevelSpreadSheet extends CreateSpreadSheet {
         super( filename, new CheckHighLevelSchema() );
     }
 
-    public void populate( Map<String, Set<String>> newPredictions, Map<String,String> labels, int size ) throws Exception {
+    public void populate( Map<String, Set<String>> newPredictions, Map<String, String> labels, int size )
+            throws Exception {
 
         List<String> datasets = new LinkedList<String>( newPredictions.keySet() );
-        
-        //sort then shuffle so we can re-create the file
-        Collections.sort(datasets);
 
-        Collections.shuffle(datasets, new Random(1));
-        
+        // sort then shuffle so we can re-create the file
+        Collections.sort( datasets );
+
+        Collections.shuffle( datasets, new Random( 1 ) );
+
         System.out.println( datasets );
         // pick random ones
         // through the set to list operations, assume it is random
@@ -36,31 +42,24 @@ public class CheckHighLevelSpreadSheet extends CreateSpreadSheet {
             String dataset = datasets.get( i );
             // ExcelUtil.setValue( spreadsheet, row, schema.getPosition( "Dataset" ), dataset );
             // for each URL
-            
-            //if its empty don't do it
-            if (newPredictions.get( dataset ).isEmpty()) {
+
+            // if its empty don't do it
+            if ( newPredictions.get( dataset ).isEmpty() ) {
                 size++;
                 row--;
             }
             for ( String URL : newPredictions.get( dataset ) ) {
                 row++;
-                ExcelUtil.setFormula( spreadsheet, row, schema.getPosition( "Dataset" ), "HYPERLINK(\"http://bioinformatics.ubc.ca/Gemma/expressionExperiment/showExpressionExperiment.html?id="+dataset+"\",\""+ dataset+"\")");
+                ExcelUtil.setFormula( spreadsheet, row, schema.getPosition( "Dataset" ),
+                        "HYPERLINK(\"http://bioinformatics.ubc.ca/Gemma/expressionExperiment/showExpressionExperiment.html?id="
+                                + dataset + "\",\"" + dataset + "\")" );
                 ExcelUtil.setValue( spreadsheet, row, schema.getPosition( "URL" ), URL );
-                ExcelUtil.setValue( spreadsheet, row, schema.getPosition( "Name" ), labels.get(URL));
-                
+                ExcelUtil.setValue( spreadsheet, row, schema.getPosition( "Name" ), labels.get( URL ) );
+
             }
 
         }
     }
 
-    /**
-     * @param args
-     */
-    public static void main( String[] args ) throws Exception {
-        // CheckHighLevelSpreadSheet test = new CheckHighLevelSpreadSheet( "ttx.xls" );
-        // test.save();
-        // log.info( "Done!" );
-
-    }
 
 }
