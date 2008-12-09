5d6e81b2b3326613d5e770dbab8b27c90bd7616c
lfrench <>
2008-12-09 19:44:09 +0000

    Setup VocabCharacteristic

--- src/main/java/ubic/GEOMMTx/AnnotateExperimentPipeLine.java
+++ src/main/java/ubic/GEOMMTx/AnnotateExperimentPipeLine.java
@@ -43,10 +43,6 @@ public class AnnotateExperimentPipeLine extends AbstractSpringAwareCLI {
     
     protected void processOptions() {
         super.processOptions();
-        if ( this.hasOption( 'd' ) ) {
-            //this.experimentID = this.getOptionValue( 'e' );
-        }
-
     }
 
     @Override
@@ -176,32 +172,16 @@ public class AnnotateExperimentPipeLine extends AbstractSpringAwareCLI {
         } catch ( Exception e ) {
             log.warn( "Couldnt load labels" );
         }
+        PredictedCharacteristicFactory charGen = new PredictedCharacteristicFactory(labels);
 
         // The call that does all the work
         Set<String> predictedAnnotations = getAnnotations( experiment );
 
         for ( String URI : predictedAnnotations ) {
             System.out.println( labels.get( URI ) + " - " + URI );
+            
+            VocabCharacteristic c = charGen.getCharacteristic( URI );
 
-            VocabCharacteristic c = VocabCharacteristic.Factory.newInstance();
-            c.setValueUri( URI );
-            c.setValue( labels.get( URI ) );
-
-            String category = null;
-            if ( URI.contains( "/owl/FMA#" ) || URI.contains( "BIRNLex-Anatomy" ) ) {
-                category = "OrganismPart";
-            }
-            if ( URI.contains( "/owl/DOID#" ) ) {
-                category = "DiseaseState";
-            }
-            if ( category != null ) {
-                c.setCategory( category );
-                c.setCategory( MgedOntologyService.MGED_ONTO_BASE_URL + category );
-            }
-            System.out.println( "Predicted category:" + category );
-            c.setEvidenceCode( GOEvidenceCode.IEA );
-            // audit trail?
-            // experiment.getCharacteristics().add( c );
         }
 
         // System.out.println( "Time todo first:" + ( System.currentTimeMillis() - time ) );
