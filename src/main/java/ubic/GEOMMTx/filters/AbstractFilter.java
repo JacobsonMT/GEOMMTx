d017ab926708fa826c09e9e9e5111800f4b4b5f6
lfrench <>
2008-12-09 19:53:14 +0000

    refactored - removed main method

--- src/main/java/ubic/GEOMMTx/filters/AbstractFilter.java
+++ src/main/java/ubic/GEOMMTx/filters/AbstractFilter.java
@@ -59,28 +59,5 @@ public abstract class AbstractFilter {
 
     public abstract String getName();
 
-    public static void main( String argsp[] ) throws Exception {
-        //Model model = ProjectRDFModelTools.loadModel( "mergedRDFBirnLexUpdate.afterrejected.rdf" );
-        Model model = ProjectRDFModelTools.loadModel( "mergedRDFBirnLexUpdate.afterrejected.testing.rdf" );
-
-        // test
-        List<AbstractFilter> filters = new LinkedList<AbstractFilter>();
-        //filters.add( new ExperimentalFactorFilter() );
-//        filters.add( new CUISUIFilter() );
-//        filters.add( new CUIIRIFilter() );
-//        filters.add( new BIRNLexFMANullsFilter() );
-        filters.add( new FrequentFilter() );
-        // low score filter not used
-
-        for ( AbstractFilter filter : filters ) {
-            log.info( "Mentions:" + ProjectRDFModelTools.getMentionCount( model ) );
-            log.info( "Running: " + filter.getName() );
-            int result = filter.filter( model );
-            log.info( "Removed: " + result );
-        }
-        model.write( new FileWriter( "mergedRDFBirnLexUpdate.afterUseless.rdf" ) );
-        log.info( "Final Mentions:" + ProjectRDFModelTools.getMentionCount( model ) );
-
-    }
 
 }
