ceee745cb2bccdbe7f5475845d3194af3609f437
lfrench <>
2009-01-12 01:03:11 +0000

    added code to check tags against review of 100 experiments

--- src/main/java/ubic/GEOMMTx/gemmaDependent/AnnotateExperimentPipeLine.java
+++ src/main/java/ubic/GEOMMTx/gemmaDependent/AnnotateExperimentPipeLine.java
@@ -31,6 +31,7 @@ import org.apache.commons.logging.LogFactory;
 import ubic.GEOMMTx.LabelLoader;
 import ubic.GEOMMTx.ProjectRDFModelTools;
 import ubic.GEOMMTx.Text2Owl;
+import ubic.GEOMMTx.evaluation.CheckHighLevelSpreadSheetReader;
 import ubic.GEOMMTx.filters.AbstractFilter;
 import ubic.GEOMMTx.filters.BIRNLexFMANullsFilter;
 import ubic.GEOMMTx.filters.CUIIRIFilter;
@@ -50,216 +51,219 @@ import ubic.gemma.ontology.OntologyService;
 import com.hp.hpl.jena.rdf.model.Model;
 
 /**
- * A class that starts with a experiment ID and in the end outputs the predicted
- * annotation URL's, after filtering
+ * A class that starts with a experiment ID and in the end outputs the predicted annotation URL's, after filtering
  * 
  * @author leon
- * @version $Id: AnnotateExperimentPipeLine.java,v 1.3 2009/01/07 18:06:04 paul
- *          Exp $
+ * @version $Id$
  */
-public class AnnotateExperimentPipeLine extends
-		ExpressionExperimentManipulatingCLI {
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		AnnotateExperimentPipeLine p = new AnnotateExperimentPipeLine();
+public class AnnotateExperimentPipeLine extends ExpressionExperimentManipulatingCLI {
+    /**
+     * @param args
+     */
+    public static void main( String[] args ) {
+        AnnotateExperimentPipeLine p = new AnnotateExperimentPipeLine();
 
-		try {
-			Exception ex = p.doWork(args);
-			if (ex != null) {
-				ex.printStackTrace();
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+        try {
+            Exception ex = p.doWork( args );
+            if ( ex != null ) {
+                ex.printStackTrace();
+            }
+        } catch ( Exception e ) {
+            throw new RuntimeException( e );
+        }
+    }
 
-	private Text2Owl text2Owl;
-	protected static Log log = LogFactory
-			.getLog(AnnotateExperimentPipeLine.class);
-	boolean loadOntologies = false;
+    private Text2Owl text2Owl;
+    protected static Log log = LogFactory.getLog( AnnotateExperimentPipeLine.class );
+    boolean loadOntologies = false;
 
-	private List<AbstractFilter> filters;
+    private List<AbstractFilter> filters;
 
-	public AnnotateExperimentPipeLine() {
-		// init MMTx
-		text2Owl = new Text2Owl();
-		text2Owl.addMapper(new BirnLexMapper());
-		text2Owl.addMapper(new FMALiteMapper());
-		text2Owl.addMapper(new DiseaseOntologyMapper());
+    public AnnotateExperimentPipeLine() {
+        // init MMTx
+        text2Owl = new Text2Owl();
+        text2Owl.addMapper( new BirnLexMapper() );
+        text2Owl.addMapper( new FMALiteMapper() );
+        text2Owl.addMapper( new DiseaseOntologyMapper() );
 
-		try {
-			filters = new LinkedList<AbstractFilter>();
-			filters.add(new CUISUIFilter());
-			filters.add(new CUIIRIFilter());
-			filters.add(new BIRNLexFMANullsFilter());
-			filters.add(new UninformativeFilter());
-		} catch (Exception e) {
-			// TODO
-			e.printStackTrace();
-			log.error(e.getMessage());
-		}
-	}
+        try {
+            filters = new LinkedList<AbstractFilter>();
+            filters.add( new CUISUIFilter() );
+            filters.add( new CUIIRIFilter() );
+            filters.add( new BIRNLexFMANullsFilter() );
+            filters.add( new UninformativeFilter() );
+        } catch ( Exception e ) {
+            // TODO
+            e.printStackTrace();
+            log.error( e.getMessage() );
+        }
+    }
 
-	/*
-	 * clears the cache of the MMTx runner, usefull for benchmarking multiple
-	 * runs
-	 */
-	public void clearMMTxCache() {
-		text2Owl.clearCache();
-	}
+    /*
+     * clears the cache of the MMTx runner, usefull for benchmarking multiple runs
+     */
+    public void clearMMTxCache() {
+        text2Owl.clearCache();
+    }
 
-	public Set<String> getAnnotations(ExpressionExperiment e) {
-		long time = System.currentTimeMillis();
+    public Set<String> getAnnotations( ExpressionExperiment e ) {
+        long time = System.currentTimeMillis();
 
-		Set<String> finalAnnotations;
+        Set<String> finalAnnotations;
 
-		ExpressionExperimentAnntotator experimentAnn = new ExpressionExperimentAnntotator(
-				e, text2Owl);
+        ExpressionExperimentAnntotator experimentAnn = new ExpressionExperimentAnntotator( e, text2Owl );
 
-		// go through each text source one by one
-		try {
-			log.info("getName()");
-			experimentAnn.annotateName();
-			// experimentAnn.writeModel();
+        // go through each text source one by one
+        try {
+            log.info( "getName()" );
+            experimentAnn.annotateName();
+            // experimentAnn.writeModel();
 
-			log.info("getDescription()");
-			experimentAnn.annotateDescription();
-			// experimentAnn.writeModel();
+            log.info( "getDescription()" );
+            experimentAnn.annotateDescription();
+            // experimentAnn.writeModel();
 
-			log.info("Publications");
-			experimentAnn.annotateReferences();
-			// experimentAnn.writeModel();
+            log.info( "Publications" );
+            experimentAnn.annotateReferences();
+            // experimentAnn.writeModel();
 
-			log.info("iterate BioAssays");
-			experimentAnn.annotateBioAssays();
-			// experimentAnn.writeModel();
+            log.info( "iterate BioAssays" );
+            experimentAnn.annotateBioAssays();
+            // experimentAnn.writeModel();
 
-		} catch (Exception ee) {
-			ee.printStackTrace();
-			log.error(ee.getMessage());
-		}
+        } catch ( Exception ee ) {
+            ee.printStackTrace();
+            log.error( ee.getMessage() );
+        }
 
-		// all the above calls for each text source builds a RDF model
-		Model model = experimentAnn.getModel();
+        // all the above calls for each text source builds a RDF model
+        Model model = experimentAnn.getModel();
 
-		// apply the filters one by one
-		for (AbstractFilter filter : filters) {
-			// log.info( "Mentions:" + Text2OwlModelTools.getMentionCount( model
-			// ) );
-			// log.info( "Running: " + filter.getName() );
-			int result = filter.filter(model);
-			// log.info( "Removed: " + result );
-		}
-		log.info("Final Mentions:"
-				+ ProjectRDFModelTools.getMentionCount(model));
+        // apply the filters one by one
+        for ( AbstractFilter filter : filters ) {
+            // log.info( "Mentions:" + Text2OwlModelTools.getMentionCount( model
+            // ) );
+            // log.info( "Running: " + filter.getName() );
+            int result = filter.filter( model );
+            // log.info( "Removed: " + result );
+        }
+        log.info( "Final Mentions:" + ProjectRDFModelTools.getMentionCount( model ) );
 
-		// write the file somewhere? we may also want to write the file before
-		// it's filtered
-		try {
-			experimentAnn.writeModel();
-		} catch (Exception ee) {
-			ee.printStackTrace();
-			log.error(ee.getMessage());
-		}
-		log.info(((System.currentTimeMillis() - time) / 1000)
-				+ "s for whole experiment");
+        // write the file somewhere? we may also want to write the file before
+        // it's filtered
+        try {
+            experimentAnn.writeModel();
+        } catch ( Exception ee ) {
+            ee.printStackTrace();
+            log.error( ee.getMessage() );
+        }
+        log.info( ( ( System.currentTimeMillis() - time ) / 1000 ) + "s for whole experiment" );
 
-		// convert the mentions into annotations using a sparql query
-		finalAnnotations = ProjectRDFModelTools.getURLsFromSingle(model);
+        // convert the mentions into annotations using a sparql query
+        finalAnnotations = ProjectRDFModelTools.getURLsFromSingle( model );
 
-		return finalAnnotations;
-	}
+        return finalAnnotations;
+    }
 
-	@Override
-	protected Exception doWork(String[] args) {
+    @Override
+    protected Exception doWork( String[] args ) {
 
-		Exception err = processCommandLine(
-				"Expression experiment annotator pipeline", args);
-		if (err != null)
-			return err;
+        Exception err = processCommandLine( "Expression experiment annotator pipeline", args );
+        if ( err != null ) return err;
 
-		ExpressionExperimentService ees = (ExpressionExperimentService) this
-				.getBean("expressionExperimentService");
-		OntologyService os = (OntologyService) this.getBean("ontologyService");
+        ExpressionExperimentService ees = ( ExpressionExperimentService ) this.getBean( "expressionExperimentService" );
+        OntologyService os = ( OntologyService ) this.getBean( "ontologyService" );
 
-		long time = System.currentTimeMillis();
-		time = System.currentTimeMillis();
+        long time = System.currentTimeMillis();
+        time = System.currentTimeMillis();
 
-		// get the rdfs:labels for the URI's
-		Map<String, String> labels = null;
-		try {
-			labels = LabelLoader.readLabels();
-		} catch (Exception e) {
-			return e;
-		}
+        // get the rdfs:labels for the URI's
+        Map<String, String> labels = null;
+        try {
+            labels = LabelLoader.readLabels();
+        } catch ( Exception e ) {
+            return e;
+        }
 
-		for (BioAssaySet bas : this.expressionExperiments) {
+        // use the HighLevel Review of 100 experiments.xls spreadsheet
+        CheckHighLevelSpreadSheetReader highLevelResults = new CheckHighLevelSpreadSheetReader();
+        Map<String, Set<String>> rejectedFromReview;
+        try {
+            rejectedFromReview = highLevelResults.getRejectedAnnotations();
+        } catch ( Exception e ) {
+            return e;
+        }
 
-			log.info("Processing: " + bas);
+        for ( BioAssaySet bas : this.expressionExperiments ) {
 
-			ExpressionExperiment experiment = (ExpressionExperiment) bas;
-			ees.thawLite(experiment);
+            log.info( "Processing: " + bas );
 
-			// construct a factory for producing VocabCharacteristics
-			PredictedCharacteristicFactory charGen = new PredictedCharacteristicFactory(
-					labels);
+            ExpressionExperiment experiment = ( ExpressionExperiment ) bas;
+            ees.thawLite( experiment );
 
-			// The call that does all the work, it gets the predicted
-			// annotations
-			Set<String> predictedAnnotations = getAnnotations(experiment);
+            // construct a factory for producing VocabCharacteristics
+            PredictedCharacteristicFactory charGen = new PredictedCharacteristicFactory( labels );
 
-			Collection<Characteristic> characteristics = experiment
-					.getCharacteristics();
-			Collection<String> alreadyHas = new HashSet<String>();
-			for (Characteristic ch : characteristics) {
-				// alreadyHas.add(ch.getValue());
-				if (ch instanceof VocabCharacteristic) {
-					String valueUri = ((VocabCharacteristic) ch).getValueUri();
-					alreadyHas.add(valueUri);
-				}
-			}
+            // The call that does all the work, it gets the predicted
+            // annotations
+            Set<String> predictedAnnotations = getAnnotations( experiment );
 
-			// for each URI print it and it's label and get VocabCharacteristic
-			// to represent it
-			Collection<Characteristic> newChars = new HashSet<Characteristic>();
-			for (String URI : predictedAnnotations) {
+            Collection<Characteristic> characteristics = experiment.getCharacteristics();
+            Collection<String> alreadyHas = new HashSet<String>();
+            for ( Characteristic ch : characteristics ) {
+                // alreadyHas.add(ch.getValue());
+                if ( ch instanceof VocabCharacteristic ) {
+                    String valueUri = ( ( VocabCharacteristic ) ch ).getValueUri();
+                    alreadyHas.add( valueUri );
+                }
+            }
 
-				if (alreadyHas.contains(URI)) {
-					log.info("Experiment already has tag " + labels.get(URI)
-							+ ", skipping");
-					continue;
-				}
+            Collection<String> rejectedBy100Eval = rejectedFromReview.get( experiment.getId() + "" );
 
-				if (!labels.containsKey(URI)) {
-					log.warn("No label for " + URI + ", skipping");
-					continue;
-				}
+            // for each URI print it and it's label and get VocabCharacteristic
+            // to represent it
+            Collection<Characteristic> newChars = new HashSet<Characteristic>();
+            for ( String URI : predictedAnnotations ) {
 
-				Characteristic c = charGen.getCharacteristic(URI);
+                if ( alreadyHas.contains( URI ) ) {
+                    log.info( "Experiment already has tag " + labels.get( URI ) + ", skipping" );
+                    continue;
+                }
 
-				if (c.getCategory() == null) {
-					log.info("No category for " + URI + ", skipping");
-					continue;
-				}
+                if ( !labels.containsKey( URI ) ) {
+                    log.warn( "No label for " + URI + ", skipping" );
+                    continue;
+                }
 
-				log.info(experiment + " -> " + labels.get(URI) + " - " + URI);
+                if ( rejectedBy100Eval != null && rejectedBy100Eval.contains( URI ) ) {
+                    log.info( "Tag was Rejected by previous review of 100 experiments " + labels.get( URI )
+                            + " "+ URI + ", skipping" );
+                    continue;
+                }
 
-				newChars.add(c);
+                Characteristic c = charGen.getCharacteristic( URI );
 
-			}
-			// attach the Characteristic to the experiment
-			log.info("Saving " + newChars.size());
-			os.saveExpressionExperimentStatements(newChars, experiment);
-		}
-		log.info("Total Time:" + (System.currentTimeMillis() - time) + "ms");
-		return null;
-	}
+                if ( c.getCategory() == null ) {
+                    log.info( "No category for " + URI + ", skipping" );
+                    continue;
+                }
 
-	@Override
-	protected void processOptions() {
-		super.processOptions();
-	}
+                log.info( experiment + " -> " + labels.get( URI ) + " - " + URI );
+
+                newChars.add( c );
+
+            }
+            // attach the Characteristic to the experiment
+            log.info( "Saving " + newChars.size() );
+            // os.saveExpressionExperimentStatements(newChars, experiment);
+        }
+        log.info( "Total Time:" + ( System.currentTimeMillis() - time ) + "ms" );
+        return null;
+    }
+
+    @Override
+    protected void processOptions() {
+        super.processOptions();
+    }
 
 }
