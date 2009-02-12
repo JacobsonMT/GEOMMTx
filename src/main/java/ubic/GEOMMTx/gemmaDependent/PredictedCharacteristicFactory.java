1e3a2ce289fca7f4f878e72cc96895d44bc8da61
lfrench <>
2009-02-12 22:16:55 +0000

    added code to set FMA annotation categories to compound if it is a biological macromolecule

--- src/main/java/ubic/GEOMMTx/gemmaDependent/PredictedCharacteristicFactory.java
+++ src/main/java/ubic/GEOMMTx/gemmaDependent/PredictedCharacteristicFactory.java
@@ -18,55 +18,86 @@
  */
 package ubic.GEOMMTx.gemmaDependent;
 
+import java.util.Collection;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import ubic.GEOMMTx.filters.BIRNLexFMANullsFilter;
 import ubic.gemma.model.association.GOEvidenceCode;
 import ubic.gemma.model.common.description.VocabCharacteristic;
+import ubic.gemma.ontology.FMAOntologyService;
 import ubic.gemma.ontology.MgedOntologyService;
+import ubic.gemma.ontology.OntologyService;
+import ubic.gemma.ontology.OntologyTerm;
 
 /**
  * @author leon, paul
- * @version $Id: PredictedCharacteristicFactory.java,v 1.3 2009/01/07 18:06:04
- *          paul Exp $
+ * @version $Id$
  */
 public class PredictedCharacteristicFactory {
-	Map<String, String> labels;
-	protected static Log log = LogFactory
-			.getLog(PredictedCharacteristicFactory.class);
+    Map<String, String> labels;
+    OntologyTerm fmaMolecule;
+    OntologyService FMAOS;
 
-	/**
-	 * Constructor requires a label map in the format of URI -> label
-	 * 
-	 * @param labels
-	 */
-	public PredictedCharacteristicFactory(Map<String, String> labels) {
-		this.labels = labels;
-	}
+    protected static Log log = LogFactory.getLog( PredictedCharacteristicFactory.class );
 
-	public VocabCharacteristic getCharacteristic(String URI) {
-		VocabCharacteristic c = VocabCharacteristic.Factory.newInstance();
-		c.setValueUri(URI);
-		c.setValue(labels.get(URI));
+    /**
+     * Constructor requires a label map in the format of URI -> label
+     * 
+     * @param labels
+     * @param FMAOS ontology service with FMA loaded
+     */
+    public PredictedCharacteristicFactory( Map<String, String> labels, OntologyService FMAOS ) {
+        // ontology serv
+        this.FMAOS = FMAOS;
+        this.labels = labels;
 
-		// infer the category
-		String category = null;
-		if (URI.contains("/owl/FMA#") || URI.contains("BIRNLex-Anatomy")) {
-			category = "OrganismPart";
-		} else if (URI.contains("/owl/DOID#")) {
-			category = "DiseaseState";
-		} else {
-			log.debug("Could not infer category for : " + URI);
-		}
+        // term for Biological macromolecule in FMA (FMAID=63887)
+        fmaMolecule = FMAOS.getTerm( "http://purl.org/obo/owl/FMA#FMA_63887" );
 
-		c.setCategory(category);
-		c.setCategoryUri(MgedOntologyService.MGED_ONTO_BASE_URL + "#"
-				+ category);
+    }
 
-		c.setEvidenceCode(GOEvidenceCode.IEA);
+    public VocabCharacteristic getCharacteristic( String URI ) {
+        VocabCharacteristic c = VocabCharacteristic.Factory.newInstance();
+        c.setValueUri( URI );
+        c.setValue( labels.get( URI ) );
 
-		return c;
-	}
+        String category = getCategory( URI );
+
+        c.setCategory( category );
+        c.setCategoryUri( MgedOntologyService.MGED_ONTO_BASE_URL + "#" + category );
+
+        c.setEvidenceCode( GOEvidenceCode.IEA );
+
+        return c;
+    }
+
+    public String getCategory( String URI ) {
+        // infer the category
+        String category = null;
+
+        if ( URI.contains( "/owl/FMA#" ) ) {
+            OntologyTerm term = FMAOS.getTerm( URI );
+
+            boolean direct = false; // get all parents
+            Collection<OntologyTerm> parents = term.getParents( direct );
+            
+            // test if its a Biological macromolecule in FMA
+            if ( parents.contains( fmaMolecule ) ) {
+                log.info( "URI is biological macromolecule in FMA" );
+                category = "Compound";
+            } else {
+                category = "OrganismPart";
+            }
+        } else if ( URI.contains( "BIRNLex-Anatomy" ) ) {
+            category = "OrganismPart";
+        } else if ( URI.contains( "/owl/DOID#" ) ) {
+            category = "DiseaseState";
+        } else {
+            log.debug( "Could not infer category for : " + URI );
+        }
+        return category;
+    }
 }
